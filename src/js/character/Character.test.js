aefb171cc8e48fa85680874d5b57139e02ea8a44
Nikita <dragon768@mail.ru>
2022-01-22 23:53:57 +0500

    Add Task

--- src/js/character/Character.test.js
+++ src/js/character/Character.test.js
@@ -1,6 +1,7 @@
 import Character from './Character';
 import * as GameClasses from './Classes';
 
+
 test('Dont create Character class', () => {
   const err = () => new Character(4);
   expect(err).toThrow("Don't create Character(), use Classes");
